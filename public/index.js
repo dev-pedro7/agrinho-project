a6c9ffe654abd5aaf3a2266bacb807de0010191f
dev-pedro7 <pedr2046@gmail.com>
2024-07-18 20:44:59 -0300

    feat: buttom toggle theme

--- public/index.js
+++ public/index.js
@@ -1,6 +1,5 @@
-// import { showTabs } from '../src/components/Menu.js';
-// window.showTabs = showTabs;
  function showTabs() {
+  if (window.innerWidth <= 910){
     var x = document.getElementById("links");
     if (x.style.display === "block") {
       x.style.display = "none";
@@ -8,3 +7,27 @@
       x.style.display = "block";
     }
   }
+}
+
+const changeTheme = document.querySelector('#change-theme')
+
+function darkMode() {
+  document.body.classList.toggle('dark')
+}
+
+function load(){
+  const darkTh = localStorage.getItem('dark')
+  if (darkTh){
+    darkMode()
+  }
+}
+load()
+
+changeTheme.addEventListener('change', function(){
+  darkMode()
+  localStorage.removeItem('dark')
+
+    if (document.body.classList.contains('dark')){
+      localStorage.setItem('dark',1)
+    }
+})
