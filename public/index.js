73e3406cc8b861e5ef0e3787503c85912548536e
dev-pedro7 <pedr2046@gmail.com>
2024-08-05 23:20:15 -0300

    feat: modal window 1.0

--- public/index.js
+++ public/index.js
@@ -66,3 +66,13 @@ function openTab(tabName) {
   });
   document.getElementById(tabName).classList.add('active');
 }
+
+function openModal(){
+  const modal = document.getElementById('modal-window')
+  modal.classList.add('open')
+  modal.addEventListener('click',(e)=>{
+    if (e.target.id =='close' || e.target.id == 'modal-window'){
+      modal.classList.remove('open')
+    }
+  })
+}
